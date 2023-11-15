bcbd5f822478dd1423ed8a976738bc01f9f03e05
Mikaela <G00401392@atu.ie>
2023-11-15 14:27:54 +0000

    Created new project

---
+++ src/main/java/ie/atu/lab5_validation/Lab5ValidationApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.lab5_validation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab5ValidationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab5ValidationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/lab5_validation/Lab5ValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.lab5_validation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab5ValidationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
