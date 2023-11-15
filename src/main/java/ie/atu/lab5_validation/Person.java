df4eb6bf53852207f2fa39cabb393ad3dd6aff06
Mikaela <G00401392@atu.ie>
2023-11-15 14:33:49 +0000

    Added fields to the Person class

--- src/main/java/ie/atu/lab5_validation/Person.java
+++ src/main/java/ie/atu/lab5_validation/Person.java
@@ -1,4 +1,67 @@
 package ie.atu.lab5_validation;
 
 public class Person {
+    private String name;
+    private String title;
+    private int employeeID;
+    private int age;
+    private String email;
+    private String position;
+    private String department;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getEmployeeID() {
+        return employeeID;
+    }
+
+    public void setEmployeeID(int employeeID) {
+        this.employeeID = employeeID;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
 }
